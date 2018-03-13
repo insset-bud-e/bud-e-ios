6df6d64e35a93cd7c74ef47af371be84b78f88ab
Pierre Joube <pierre.joube@gmail.com>
2018-03-13 13:40:38 +0100

    initial commit

---
+++ bud-e/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bud-e
+//
+//  Created by Pierre Joube on 13/03/2018.
+//  Copyright © 2018 Pierre Joube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
